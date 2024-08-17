0c27b7f92fe22e5f8dfc13a1490c9265d55df608
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-08-17 15:15:24 +0200

    Phase 1 Fixes useTexture error

--- src/components/LandingPage/NewestWork/Model/index.jsx
+++ src/components/LandingPage/NewestWork/Model/index.jsx
@@ -8,19 +8,6 @@ import { fragment, vertex } from "../Shader";
 import { motion } from "framer-motion-3d";
 import { NewestProjects } from "@/constants";
 
-// const is causing error because the images are not loaded yet and the useTexture is used in callback function.
-function useProjectTextures(projects) {
-    const [textures, setTextures] = useState([]);
-
-    useEffect(() => {
-        // Load textures asynchronously and update state
-        Promise.all(projects.map(project => useTexture(project.src)))
-               .then(setTextures);
-    }, [projects]); // Re-run when `projects` changes
-
-    return textures;
-}
-
 // we have passed the activeProject as a prop to the model component and then we need to show it individually
 export default function Model ({ activeProject, containerRef }) {
     // Refs
@@ -38,7 +25,7 @@ export default function Model ({ activeProject, containerRef }) {
     // Textures
     // const textures = NewestProjects.map((project, i)=> useTexture(project.src)); // creating a texture for the component - need to access the project images
 
-    const textures = useProjectTextures(NewestProjects);
+    const textures = projects.map((project, i)=> useTexture(project.src)); // creating a texture for the component - need to access the project images
 
     // Motion values
     const smoothMouse = {
