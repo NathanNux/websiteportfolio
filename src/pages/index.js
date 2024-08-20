235161189a197dbb6a8d86e7078fc6178213dfa4
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-08-20 22:47:38 +0200

    fixin scss bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,17 @@ export default function Home() {
         <Slider />
         <NewestWork />
         <IntroText phrases={phrases}/>
-        <OfferSection />
+        <ZoomParallax 
+          src1='/assets/images/projects/components.png' 
+          src2='/assets/images/slider-la/seo.png' 
+          src3='/assets/images/slider-la/coding.png' 
+          src4='/assets/images/slider-la/performance.png' 
+          src5='/assets/images/slider-la/color-pallet.png' 
+          src6='/assets/images/slider-la/offer.png' 
+          src7='/assets/images/slider-la/research.png' 
+          path='/assets/images/slider-la/videos/main.mp4'
+          text='Originalita - Skvělý Design - Skvělá Nabídka - Prodeje -'
+        />
         <MainOffer />
         <FreeOffers text='1'/>
         <Footer />
