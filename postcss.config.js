4d1d70a32a76a38fea9fd95923663bb32f022046
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-08-31 19:20:26 +0200

    modifying postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,22 +2,6 @@ module.exports = {
   plugins: [
     'tailwindcss',
     'autoprefixer',
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          [
-            '@fullhuman/postcss-purgecss',
-            {
-              content: [
-                './pages/**/*.{js,jsx,ts,tsx}',
-                './components/**/*.{js,jsx,ts,tsx}',
-                // Add other directories where you use Tailwind CSS or SCSS
-              ],
-              defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-              safelist: ['html', 'body'], // Adjust the safelist as needed
-            },
-          ],
-        ]
-      : []),
   ],
 };
 // this setup is for Next.js, but you can adjust it for other frameworks
