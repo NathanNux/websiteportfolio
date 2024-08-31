5a92d713b4636dccfd8ee98626eac4fbf515cdcc
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-08-31 19:14:09 +0200

    fixing post css config

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,24 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    // Add other directories where you use Tailwind CSS or SCSS
-  ],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  safelist: ['html', 'body'], // Adjust the safelist as needed
-});
-
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          [
+            '@fullhuman/postcss-purgecss',
+            {
+              content: [
+                './pages/**/*.{js,jsx,ts,tsx}',
+                './components/**/*.{js,jsx,ts,tsx}',
+                // Add other directories where you use Tailwind CSS or SCSS
+              ],
+              defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+              safelist: ['html', 'body'], // Adjust the safelist as needed
+            },
+          ],
+        ]
+      : []),
   ],
 };
+// this setup is for Next.js, but you can adjust it for other frameworks
+// it will remove unused CSS in production thus making better ratings in Lighthouse
