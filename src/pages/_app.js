13dc499dbc9f33d35f23f55b6d9529c58af75187
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-09-04 14:36:14 +0200

    adding analytics and speed controls with sentry for debuging

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,39 +5,40 @@ import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react"; // Import the Analytics component
+import { SpeedInsights } from "@vercel/speed-insights/next"; // Import the SpeedInsights component
 
 export default function App({ Component, pageProps, router }) {
-
-  //now i need o put all the content inside the AnimatePresence component to make the animations work
-
   const pathname = usePathname();
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            window.scrollTo({
-                top: 0,
-                left: 0,
-                behavior: 'smooth'
-            });
-        }, 1000)
+  // Now I need to put all the content inside the AnimatePresence component to make the animations work
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }, 1000);
 
-        return () => {
-            clearTimeout(timer);
-        }
-    }, [pathname])
-
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [pathname]);
 
   return (
-  <LoadProvider>
-    <Head>
-    <meta name="description" content="Personal Portfolio Website" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-      <title>Matěj Forejt</title>
-    </Head>
-    <AnimatePresence mode="wait">
-      <Component key={router.route} {...pageProps} />
-    </AnimatePresence>
-  </LoadProvider>
-)
+    <LoadProvider>
+      <Head>
+        <meta name="description" content="Personal Portfolio Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Matěj Forejt</title>
+      </Head>
+      <AnimatePresence mode="wait">
+        <Component key={router.route} {...pageProps} />
+        <Analytics /> 
+        <SpeedInsights /> 
+      </AnimatePresence>
+    </LoadProvider>
+  );
 }
