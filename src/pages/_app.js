ee08786fde0f72e208b237ea87d519288bd7d0fa
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-09-04 14:53:01 +0200

    hotfixing vercel add-ons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import Head from "next/head";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps, router }) {
