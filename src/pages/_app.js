6a82a3bb16dd0768903e1e88b2c3142c856f52d0
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-09-04 14:57:23 +0200

    looking for bug in the app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,8 +5,6 @@ import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import Head from "next/head";
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps, router }) {
   const pathname = usePathname();
@@ -36,8 +34,6 @@ export default function App({ Component, pageProps, router }) {
       </Head>
       <AnimatePresence mode="wait">
         <Component key={router.route} {...pageProps} />
-        <Analytics /> 
-        <SpeedInsights /> 
       </AnimatePresence>
     </LoadProvider>
   );
