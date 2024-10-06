d9ad6fc4e7c4c6f49757296a170989f53228f57c
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-10-06 16:29:39 +0200

    adding Clarity script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -36,6 +36,15 @@ export default function App({ Component, pageProps, router }) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <title>Matěj Forejt - Freelancer</title>
+      <script type="text/javascript">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "oefksnfi6w");
+          `}
+        </script>
     </Head>
     <AnimatePresence mode="wait">
       <Component key={router.route} {...pageProps} />
