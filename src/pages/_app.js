c9e49906c1d8cb75e9beb67b7bd393be38d5b7c1
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-10-07 22:20:38 +0200

    Adding google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,6 @@ import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import Head from "next/head";
-import useGetLocation from "@/utils/useGetLocation";
 import Script from "next/script";
 
 export default function App({ Component, pageProps, router }) {
@@ -44,6 +43,22 @@ export default function App({ Component, pageProps, router }) {
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "oefksnfi6w");
+          `,
+        }}
+      />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-K1TBHXHC6J"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-K1TBHXHC6J');
           `,
         }}
       />
